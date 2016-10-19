b965a3bfff77cac9ef2bc94af5c9b5e2b9a7d167
Maxwell Russell <maxruss.87@gmail.com>
2016-10-19 07:57:52 -0500

    adjusted delete function more

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var toDoList = $('.toDo-list');
 var container = $('.container');
 
 function renderForm(){
-  var form = $('<form class="new-toDo">'+'<input type="text" class="newTask">'+'<button class="add">'+'<i class="fa fa-plus" aria-hidden="true"></i>'+'</button>'+'</form>');
+  var form = $('<form class="new-toDo">'
+  +'<input type="text" class="newTask">'
+  +'<button class="add">'
+  +'<i class="fa fa-plus" aria-hidden="true"></i>'
+  +'</button>'
+  +'</form>');
   container.append(form);
   form.find('.add').on('click', function(e) {
-    e.preventDefault();
     var newTask = {
       importance: 'normal',
       todo: form.find('.newTask').val(),
@@ -35,7 +39,7 @@ function renderForm(){
 function renderCurrentTasks(){
   toDoList.empty();
   header.empty();
-  var newTitle = $('<h2>Current Tasks</h2>');
+  var newTitle = $('<h1>Current Tasks</h1>');
   //display all current tasks only
   var settings = {
   		url: 'http://tiny-za-server.herokuapp.com/collections/max-todos',
@@ -44,13 +48,21 @@ function renderCurrentTasks(){
   			data.forEach(function(toDo, i, arr) {
   				// make an li + add an importance sub line if an importance is added
           if (toDo.importance === 'high' && toDo.status === 'incomplete') {
-            var newTask = $('<li class="' + toDo._id + '">' + toDo.todo + '<span><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>' + '</li><button class="delete ' + toDo._id + '"><i class="fa fa-trash" aria-hidden="true"></i></button>');
-    				toDoList.append(newTask);
-            console.log('importance added');
+            var newHighTask = $('<li class="' + toDo._id + '">' + toDo.todo +
+            '<span><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>'
+            + '<button class="delete ' + toDo._id + '">'
+            + '<i class="fa fa-trash" aria-hidden="true"></i>'
+            + '</button></li>');
+    				toDoList.append(newHighTask);
+            newHighTask.find('.delete').on('click', deleteClick(toDo._id));
+            // console.log('importance added');
           } else if (toDo.importance === 'normal' && toDo.status === 'incomplete') {
-            var newTask = $('<li class="' + toDo._id + '">' + toDo.todo + '</li><button class="delete ' + toDo._id + '"><i class="fa fa-trash" aria-hidden="true"></i></button>');
-    				toDoList.append(newTask);
-            console.log('no importance added');
+            var newNormalTask = $('<li class="' + toDo._id + '">' + toDo.todo
+            + '<button class="delete ' + toDo._id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></li>');
+    				toDoList.append(newNormalTask);
+            newNormalTask.find('.delete').on('click', deleteClick(toDo._id));
+            // find the specific and add event listener // true false vs
+            // console.log('no importance added');
           } else if (toDo.status === 'complete'){
             console.log('do not add');
           }
@@ -68,20 +80,22 @@ function renderCompleteTasks(){
   //display all completed tasks
   toDoList.empty();
   header.empty();
-  var newTitle = $('<h2>Completed Tasks</h2>');
+  var newTitle = $('<h1>Completed Tasks</h1>');
   var settings = {
       url: 'http://tiny-za-server.herokuapp.com/collections/max-todos',
       type: 'GET',
       success: function(data, status, xhr) {
         data.forEach(function(toDo, i, arr) {
           if (toDo.importance === 'high' && toDo.status === 'complete') {
-            var newTask = $('<li class="' + toDo._id + '">' + toDo.todo + '<span><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>' + '</li><button class="delete ' + toDo._id + '"><i class="fa fa-trash" aria-hidden="true"></i></button>');
-            toDoList.append(newTask);
-            console.log('importance added');
+            var newHighTask = $('<li class="' + toDo._id + '">' + toDo.todo
+            + '<span><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>'
+            + '<i class="fa fa-check" aria-hidden="true"></i></li>');
+            toDoList.append(newHighTask);
+            // console.log('importance added');
           } else if (toDo.importance === 'normal' && toDo.status === 'complete') {
-            var newTask = $('<li class="' + toDo._id + '">' + toDo.todo + '</li><button class="delete ' + toDo._id + '"><i class="fa fa-trash" aria-hidden="true"></i></button>');
-            toDoList.append(newTask);
-            console.log('no importance added');
+            var newNormalTask = $('<li class="' + toDo._id + '">' + toDo.todo + '<i class="fa fa-check" aria-hidden="true"></i></li>');
+            toDoList.append(newNormalTask);
+            // console.log('no importance added');
           }
         });
       },
@@ -93,25 +107,50 @@ function renderCompleteTasks(){
     header.append(newTitle);
 }
 
-toDoList.find('.delete').on('click', function(e) {
-  e.preventDefault();
-  // this listener should delete posts
-  var selectedButton = e.target.className;
-  console.log(selectedButton);
 
-  // var settings = {
-  //     url: 'http://tiny-za-server.herokuapp.com/collections/max-todos',
-  //     type: 'PUT',
-  //     success: function(data, status, xhr) {
-  //     },
-  //     error: function() {
-  //       console.log('could not complete post');
-  //     }
-  //   };
-  //   $.ajax(settings);
-  //
-  // status: 'incomplete'
-});
+// build a function that moves the add listener for buttons elsewhere
+
+function deleteClick(e, postID) {
+  console.log('delete ran');
+  var deletePost = postID;
+  var settings = {
+      url: 'http://tiny-za-server.herokuapp.com/collections/max-todos/' + deletePost,
+      type: 'DELETE',
+      success: function(data, status, xhr) {
+        console.log('deleted!');
+        renderCurrentTasks();
+      },
+      error: function() {
+        // log an error
+        console.log('the post didn\'t work');
+      },
+      contentType: 'application/json',
+    };
+    $.ajax(settings);
+  }
+
+
+// deleteButtons.forEach(function(button, i, arr){
+//   button.on('click', function(e) {
+//     e.preventDefault();
+//     // this listener should delete posts
+//     var selectedButton = e.target.className;
+//     console.log(selectedButton);
+//   });
+// });
+
+// var settings = {
+//     url: 'http://tiny-za-server.herokuapp.com/collections/max-todos',
+//     type: 'PUT',
+//     success: function(data, status, xhr) {
+//     },
+//     error: function() {
+//       console.log('could not complete post');
+//     }
+//   };
+//   $.ajax(settings);
+//
+// status: 'incomplete'
 
 function renderAllTasks(){
   //display all current and complete tasks
